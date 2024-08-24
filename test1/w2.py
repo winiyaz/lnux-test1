768c04ecd491956cfa862d7b82449c8db1a37560
winiyaz <winiyaz@proton.me>
2024-08-24 10:10:08 +0000

    Lick

--- test1/w2.py
+++ test1/w2.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
-# Python file for writing fiels to disk and testing
+# Python file for writing files to disk and testing
 # Taken from mistrk
 
 import os
-import json
 import subprocess
 
 # Check if the file exists
-if not os.path.exists('tt.txt'):
+if not os.path.exists('ttw2.txt'):
     print('File not found - Creating and writing to the file')
     write_pnty = ["Panty Smells", "FartNose"]
-    with open('tt.txt', 'w') as file:
-        json.dump(write_pnty, file, indent=4)
+    with open('ttw2.txt', 'w') as file:
+        for item in write_pnty:
+            file.write(item + '\n')
 
 # List file and display its contents
 subprocess.run(["ls", "-al", "tt.txt"])
